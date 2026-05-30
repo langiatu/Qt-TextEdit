a69a82d87f9321a67d6248fb9706f6f1af6d9f4b
Levi <3358247958@qq.com>
2026-05-30 13:43:25 +0800

    no_build

--- build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_TextEidt_FILE E:/QtWorkspace/TextEidt/build/Desktop_Qt_6_11_0_MinGW_64_bit-Debug/TextEidt.exe)
-set(__QT_DEPLOY_TARGET_TextEidt_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_TextEidt_RUNTIME_DLLS E:/Qt/6.11.0/mingw_64/bin/Qt6Widgets.dll;E:/Qt/6.11.0/mingw_64/bin/Qt6Gui.dll;E:/Qt/6.11.0/mingw_64/bin/Qt6Core.dll)
